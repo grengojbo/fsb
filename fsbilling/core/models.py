9e4e5850f67537c66992ac28935ee783c1dc9b05
Oleg Dolya <oleg.dolya@gmail.com>
2009-05-07 03:44:19 +0300

    add balance, tarif, service models

--- fsbilling/core/models.py
+++ fsbilling/core/models.py
@@ -1,9 +1,61 @@
 # -*- coding: UTF-8 -*-
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 
 __author__ = '$Author:$'
 __revision__ = '$Revision:$'
 
 # Create your models here.
+class TarifPlan(models.Model):
+    """(Tarif description)"""
+    name = models.CharField(_(u'Name'), max_length=80)    
+    description = models.CharField(_(u'Description'), blank=True, max_length=240)
+
+    class Meta:
+        #ordering = []
+        db_table = 'Tarif'
+        verbose_name, verbose_name_plural = _(u"Tarif Plan"), _(u"Tarif Plans")
+
+    def __unicode__(self):
+        return self.name
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('Tarif', [self.id])
+        
+class Service(models.Model):
+    """(Service description)"""
+    name = models.CharField(_(u'Name'), max_length=80)    
+    description = models.CharField(_(u'Description'), blank=True, max_length=240)
+
+    class Meta:
+        ordering = []
+        db_table = 'service'
+        verbose_name, verbose_name_plural = _(u"Service"), _(u"Services")
+
+    def __unicode__(self):
+        return self.name
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('Services', [self.id])
+
+class Balance(models.Model):
+    """(Balance description)"""
+    accauntcode = models.ForeignKey(User)
+    cash = models.FloatField(_(u"Cash"))
+    tarif_plan = models.ForeignKey(TarifPlan)
+    service = models.ManyToManyField(Service)
+
+    class Meta:
+        #ordering = []
+        verbose_name, verbose_name_plural = "Balance", "Balances"
+
+    def __unicode__(self):
+        return u"Balance"
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('Balance', [self.id])
