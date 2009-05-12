2ea6f813494859a361ba54e2c150c83b7344df7c
Oleg Dolya <oleg.dolya@gmail.com>
2009-05-12 09:31:05 +0300

    add config nibblebill

--- fsbilling/core/models.py
+++ fsbilling/core/models.py
@@ -3,11 +3,32 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
+from fsadmin.core.managers import GenericManager
 
 __author__ = '$Author:$'
 __revision__ = '$Revision:$'
 
 # Create your models here.
+class NibbleBill(models.Model):
+    """(NibbleBill description)"""
+    name = models.CharField(_(u'Name'), max_length=200)
+    server = models.ForeignKey(u'Server')
+    enabled = models.BooleanField(_(u'Enable'), default=True)
+    #active_objects = GenericManager( enabled = True ) # only active entries
+    #inactive_objects = GenericManager( enabled = False ) # only inactive entries 
+
+    class Meta:
+        #ordering = []
+        db_table = 'nibblrbill'
+        verbose_name, verbose_name_plural = _(u"Billing"), _(u"Billings")
+
+    def __unicode__(self):
+        return self.name
+
+    #@models.permalink
+    #def get_absolute_url(self):
+    #    return ('NibbleBill', [self.id])
+    
 class TarifPlan(models.Model):
     """(Tarif description)"""
     name = models.CharField(_(u'Name'), max_length=80)    
@@ -31,7 +52,7 @@ class Service(models.Model):
     description = models.CharField(_(u'Description'), blank=True, max_length=240)
 
     class Meta:
-        ordering = []
+        #ordering = []
         db_table = 'service'
         verbose_name, verbose_name_plural = _(u"Service"), _(u"Services")
 
@@ -42,20 +63,20 @@ class Service(models.Model):
     def get_absolute_url(self):
         return ('Services', [self.id])
 
-class Balance(models.Model):
-    """(Balance description)"""
-    accauntcode = models.ForeignKey(User)
-    cash = models.FloatField(_(u"Cash"))
-    tarif_plan = models.ForeignKey(TarifPlan)
-    service = models.ManyToManyField(Service)
-
-    class Meta:
-        #ordering = []
-        verbose_name, verbose_name_plural = "Balance", "Balances"
-
-    def __unicode__(self):
-        return u"Balance"
-
-    @models.permalink
-    def get_absolute_url(self):
-        return ('Balance', [self.id])
+#class Balance(models.Model):
+#    """(Balance description)"""
+#    accauntcode = models.ForeignKey(User)
+#    cash = models.FloatField(_(u"Cash"))
+#    tarif_plan = models.ForeignKey(TarifPlan)
+#    service = models.ManyToManyField(Service)
+#
+#    class Meta:
+#        #ordering = []
+#        verbose_name, verbose_name_plural = "Balance", "Balances"
+#
+#    def __unicode__(self):
+#        return u"Balance"
+#
+#    @models.permalink
+#    def get_absolute_url(self):
+#        return ('Balance', [self.id])
--- fsbilling/core/tests.py
+++ fsbilling/core/tests.py
@@ -1,12 +1,28 @@
 # -*- coding: UTF-8 -*-
+"""
+This file demonstrates two different styles of tests (one doctest and one
+unittest). These will both pass when you run "manage.py test".
+
+Replace these with more appropriate tests for your application.
+"""
+
+__author__ = '$Author$'
+__revision__ = '$Revision$'
+
+import unittest
 from django import test
-from django.core import mail
+from django.test.client import Client
 from django.contrib.auth.models import User
+from fsbilling.core.models import c
+from fsadmin.server.models import Server
+import csv, sys, os
 
-class TestCore(TestCase):
-
+class LcrTestCase(test.TestCase):
+    fixtures = ['testsite', 'alias', 'server', 'context', 'gateway', 'sipprofile', 'fsgroup', 'testendpoint', 'testcdr', 'acl']
     def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+        
+    def testCoreConf(self):
+        """docstring for billing core"""
         pass
-
-    def tearDown(self):
-        pass 
