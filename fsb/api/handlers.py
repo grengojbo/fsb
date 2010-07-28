035aa53d69b24569abf53e2e2d8ab5c1ad7daa53
Oleg Dolya <oleg.dolya@gmail.com>
2010-07-28 10:57:30 +0300

    modified:   fsb/api/handlers.py modified:   fsb/api/urls.py

--- fsb/api/handlers.py
+++ fsb/api/handlers.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 from django.contrib.sites.models import RequestSite
 from django.contrib.sites.models import Site
 from fsa.lcr.models import Lcr
+from fsb.tariff.models import Tariff
 from fsa.core.utils import pars_phone
 from threaded_multihost.threadlocals import get_current_user, set_current_user
 #from piston.doc import generate_doc
@@ -31,7 +32,7 @@ class BillingHandler(BaseHandler):
     #    return ('api_numberplan_handler', ['phone_number'])
 
     #@require_mime('json')
-    def read(self, request, account=None, phone=None, si=None):
+    def read(self, request, account=None, phone=None, si=None, tariff=None):
         """
         Returns a blogpost, if `title` is given,
         otherwise all the posts.
@@ -41,11 +42,13 @@ class BillingHandler(BaseHandler):
         """
         user = request.user
         if user.has_perm("billing.api_view"):
-            if phone is not None and si is not None:
-                lcr_query = "SELECT l.id AS id, l.digits AS digits, cg.name AS gw, l.rate AS rate, cg.prefix AS gw_prefix, cg.suffix AS suffix, l.price AS price, l.price_currency AS currency, l.name AS name FROM lcr l LEFT JOIN carrier_gateway cg ON l.carrier_id_id=cg.id LEFT JOIN django_site s ON l.site_id=s.id WHERE cg.enabled = '1' AND l.enabled = '1' AND l.digits IN ({0}) AND CURTIME() BETWEEN l.time_start AND l.time_end AND (DAYOFWEEK(NOW()) = l.weeks OR l.weeks = 0) AND s.name='{1}' ORDER BY  digits DESC, reliability DESC, quality DESC;".format(pars_phone(phone), si)
+            if phone is not None and si is not None and tariff is not None:
+                #lcr_query = "SELECT l.id AS id, l.digits AS digits, cg.name AS gw, l.rate AS rate, cg.prefix AS gw_prefix, cg.suffix AS suffix, l.price AS price, l.price_currency AS currency, l.name AS name FROM lcr l LEFT JOIN carrier_gateway cg ON l.carrier_id_id=cg.id LEFT JOIN django_site s ON l.site_id=s.id WHERE cg.enabled = '1' AND l.enabled = '1' AND l.digits IN ({0}) AND CURTIME() BETWEEN l.time_start AND l.time_end AND (DAYOFWEEK(NOW()) = l.weeks OR l.weeks = 0) AND s.name='{1}' ORDER BY  digits DESC, reliability DESC, quality DESC;".format(pars_phone(phone), si)
                 log.debug(lcr_query)
-                resp = Lcr.objects.raw(lcr_query)[0]
-                return {"lcr_rate": resp.rate, "suffix": resp.suffix, "lcr_digits": resp.digits, "lcr_carrier": resp.gw, "lcr_price": resp.price, "lcr_currency": resp.currency, "lcr_name": resp.name }
+                #resp = Lcr.objects.raw(lcr_query)[0]
+                resp = Lcr.objects.phone_lcr(phone, si)
+                respt = Tariff.objects.phone_tariff(phone, tariff)
+                return {"lcr_rate": resp.rate, "suffix": resp.suffix, "lcr_digits": resp.digits, "lcr_carrier": resp.gw, "lcr_price": resp.price, "lcr_currency": resp.currency, "lcr_name": resp.name, "nibble_rate": respt.rate }
             else:
                 return rc.NOT_HERE
         else:
