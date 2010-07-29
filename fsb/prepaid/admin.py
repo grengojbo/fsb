a6ff0aff645e9488b343b26957adb5c41581b980
Oleg Dolya <oleg.dolya@gmail.com>
2010-07-29 15:49:43 +0300

    Change Prepaid Admin view modified:   fsb/prepaid/admin.py

--- fsb/prepaid/admin.py
+++ fsb/prepaid/admin.py
@@ -10,8 +10,11 @@ from django.utils.translation import get_language, ugettext_lazy as _
 #    extra = 1
 
 class PrepaidOptions(admin.ModelAdmin):
-    list_display = ['num_prepaid', 'enabled', 'valid', 'balance', 'date_end']
+    list_display = ('num_prepaid', 'enabled', 'valid', 'balance', 'date_end', 'nt',)
     list_display_links = ('num_prepaid',)
+    search_fields = ['num_prepaid']
+    search_fields_verbose = ['Number Card']
+    list_filter = ('enabled', 'nt', 'balance',)
     ordering = ['date_added']
     #inlines = [PrepaidUsage_Inline]
 
