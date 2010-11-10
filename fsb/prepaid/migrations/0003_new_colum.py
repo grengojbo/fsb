74d909bd4bc4e50e77e362e7439dab688c5f8b95
Oleg Dolya <oleg.dolya@gmail.com>
2010-11-10 18:22:22 +0200

    Add new colum to prepaid new file:   fsb/prepaid/migrations/0003_new_colum.py modified:   fsb/prepaid/models.py

---
+++ fsb/prepaid/migrations/0003_new_colum.py
@@ -0,0 +1,80 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding field 'Prepaid.diller'
+        db.add_column('prepaid_prepaid', 'diller', self.gf('django.db.models.fields.related.ForeignKey')(default=1, to=orm['auth.User']), keep_default=False)
+
+        # Changing field 'Prepaid.code'
+        db.alter_column('prepaid_prepaid', 'code', self.gf('django.db.models.fields.CharField')(unique=True, max_length=16))
+
+
+    def backwards(self, orm):
+        
+        # Deleting field 'Prepaid.diller'
+        db.delete_column('prepaid_prepaid', 'diller_id')
+
+        # Changing field 'Prepaid.code'
+        db.alter_column('prepaid_prepaid', 'code', self.gf('django.db.models.fields.CharField')(max_length=14, unique=True))
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'prepaid.prepaid': {
+            'Meta': {'unique_together': "(('num_prepaid', 'code'),)", 'object_name': 'Prepaid'},
+            'code': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '16'}),
+            'date_added': ('django.db.models.fields.DateField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'date_end': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'diller': ('django.db.models.fields.related.ForeignKey', [], {'default': '1', 'to': "orm['auth.User']"}),
+            'enabled': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'message': ('django.db.models.fields.CharField', [], {'max_length': '254', 'null': 'True', 'blank': 'True'}),
+            'nt': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '1', 'max_length': '1'}),
+            'num_prepaid': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '12'}),
+            'start_balance': ('django.db.models.fields.DecimalField', [], {'max_digits': '8', 'decimal_places': '2'}),
+            'valid': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        }
+    }
+
+    complete_apps = ['prepaid']
--- fsb/prepaid/models.py
+++ fsb/prepaid/models.py
@@ -15,6 +15,7 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from l10n.utils import moneyfmt
 from livesettings import config_value
+from django.contrib.auth.models import User
 #from fsb.prepaid.utils import generate_certificate_code
 #from fsb.billing.models import CurrencyBase
 #from payment.utils import get_processor_by_key
@@ -174,7 +175,7 @@ class Prepaid(models.Model):
     #site = models.ForeignKey(Site, null=True, blank=True, verbose_name=_('Site'))
     #order = models.ForeignKey(Order, null=True, blank=True, related_name="prepaids", verbose_name=_('Order'))
     num_prepaid = models.CharField(_(u'Number'), max_length=12, unique=True)
-    code = models.CharField(_('Prepaid Code'), max_length=14, unique=True)
+    code = models.CharField(_('Prepaid Code'), max_length=16, unique=True)
     #purchased_by =  models.ForeignKey(Contact, verbose_name=_('Purchased by'),
     #    blank=True, null=True, related_name='prepaids_purchased')
     date_added = models.DateField(_("Date added"), auto_now_add = True)
@@ -184,6 +185,7 @@ class Prepaid(models.Model):
     valid = models.BooleanField(_('Valid'), default=False)
     message = models.CharField(_('Message'), blank=True, null=True, max_length=254)
     start_balance = models.DecimalField(_("Starting Balance"), decimal_places=2, max_digits=8)
+    diller = models.ForeignKey(User, verbose_name=_(u'Diller'), default=1)
     #currency = models.ForeignKey(CurrencyBase, default=1, related_name='currencys', verbose_name=_('Currency'))
     objects = PrepaidManager()
 
